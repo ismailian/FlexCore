e05839d216b17ee4c6d3e90373052389b7d45b34
z-offset <techcyber598@gmail.com>
2020-11-24 07:09:44 -0800

    Latest changes.

--- Core/App.php
+++ Core/App.php
@@ -3,14 +3,19 @@
 ##############################################
 ###############   Entry Point  ###############
 ##############################################
-require_once __DIR__ . '/config/Init.php';   #
+// require_once __DIR__ . '/config/Init.php';   #
 ##############################################
 
-
 ## ROUTES ##
-Router::get('home', 'index');
+// Router::get('home', 'index');
 ## ROUTES ##
 
 
-Router::
-    default();
+// Router::default();
+
+$uri = urldecode(
+    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
+);
+
+Response::ctype('json');
+print_r($uri);
--- bin/__Init.php
+++ bin/__Init.php
@@ -8,82 +8,80 @@ require_once __DIR__ . "/_lib/argParser.php";
 class Cli
 {
 
-    /**
-     * This class handles all the operations done by the CLI.
-     */
+  /**
+   * This class handles all the operations done by the CLI.
+   */
 
-    # Commands #
-    private $commands = [
-        'handler' => ['create', 'delete'],
-        'model'   => ['create', 'delete'],
-        'module'  => ['create', 'delete'],
-        'utility' => ['create', 'delete'],
-        'view'    => ['create', 'delete']
-    ];
+  # Commands #
+  private $commands = [
+    'handler' => ['create', 'delete'],
+    'model'   => ['create', 'delete'],
+    'module'  => ['create', 'delete'],
+    'utility' => ['create', 'delete'],
+    'view'    => ['create', 'delete']
+  ];
 
-    /**
-     * @description Initialized the cli.
-     * @return void
-     */
-    public static function init()
-    {
+  /**
+   * @description Initialized the cli.
+   * @return void
+   */
+  public static function init()
+  {
 
-        # parse #
-        $data = argParser::parse();
-        
+    # parse #
+    $data = argParser::parse();
 
-        # command uncounted for #
-        if (!empty($data->command) && !in_array($data->command, array_keys(Self::$commands))) throw new Exception("Command is not recognized", 0);
 
-        # command # show top commands help.
-        if (!empty($data->command)) {
+    # command uncounted for #
+    if (!empty($data->command) && !in_array($data->command, array_keys(Self::$commands))) throw new Exception("Command is not recognized", 0);
 
-            # sub command uncounted for #
-            if (!empty($data->sub_command) && !in_array($data->sub_command, Self::$commands[$data->command])) throw new Exception("Sub Command is not recognized", 0);
+    # command # show top commands help.
+    if (!empty($data->command)) {
 
-            # sub_command # show given command help.
-            if (!empty($data->sub_command)) {
+      # sub command uncounted for #
+      if (!empty($data->sub_command) && !in_array($data->sub_command, Self::$commands[$data->command])) throw new Exception("Sub Command is not recognized", 0);
 
-                # params # show given command requirements help.
-                if (!empty($data->params)) {
+      # sub_command # show given command help.
+      if (!empty($data->sub_command)) {
 
-                    # do what you have to do.
-                    // echo "CLI is to [{$data->sub_command}] a [{$data->command}] named ({$data->params})";
+        # params # show given command requirements help.
+        if (!empty($data->params)) {
 
-                    // IO handles all __callStatic => IO::{$sub_command}($params)
-                    CLI::{$data->sub_command}($data->command, $data->params);
+          # do what you have to do.
+          // echo "CLI is to [{$data->sub_command}] a [{$data->command}] named ({$data->params})";
 
-                    return;
-                }
-                die("Show [{$data->sub_command}] requirements.");
-                # params # show given command requirements help.
+          // IO handles all __callStatic => IO::{$sub_command}($params)
+          CLI::{$data->sub_command}($data->command, $data->params);
 
-                return;
-            }
-            die("Show [{$data->sub_command}] help.");
-            # sub_command # show given command help.
-
-            return;
+          return;
         }
-        die("List All Commands...");
+        die("Show [{$data->sub_command}] requirements.");
+        # params # show given command requirements help.
+
+        return;
+      }
+      die("Show [{$data->sub_command}] help.");
+      # sub_command # show given command help.
+
+      return;
+    }
+    die("List All Commands...");
+  }
+
+
+  # in arguments [scope={top_level_command}, params={data_to_manipulate}]
+  private static function __callStatic($name, $arguments)
+  {
+
+    if (in_array($name, array_keys(Self::$commands))) {
+
+      $subCommand = $arguments[0];
+      $params     = $arguments[1];
+
+      if (empty($subCommand) || empty($params)) die("[ERR] Missing Arguments.");
     }
 
-
-    # in arguments [scope={top_level_command}, params={data_to_manipulate}]
-    private static function __callStatic($name, $arguments)
-    {
-
-        if (in_array($name, array_keys(Self::$commands))) {
-                
-            $subCommand = $arguments[0];
-            $params     = $arguments[1];
-
-            if ( empty($subCommand) || empty($params) ) die("[ERR] Missing Arguments.");
-
-        }
-
-        # error out
-        die("[ERR] Command Not Recognized.");
-
-    }
+    # error out
+    die("[ERR] Command Not Recognized.");
+  }
 }
