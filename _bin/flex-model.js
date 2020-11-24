4d0739f4ca31e61a0d27fe0a0498fa4341d0b2ba
z-offset <techcyber598@gmail.com>
2020-11-24 11:07:30 -0800

    latest changes

--- _bin/flex-model.js
+++
@@ -1,8 +0,0 @@
-const program = require("commander");
-const model = require("./commands/Model");
-
-// sub-commands
-program.command("create").description("Create a Model.").action(model.create);
-program.command("delete").description("Delete a Model.").action(model.delete);
-
-program.parse(process.argv);
--- _bin/flex-view.js
+++
@@ -1,8 +0,0 @@
-const program = require("commander");
-const view = require("./commands/View");
-
-// sub-commands
-program.command("create").description("Create a View.").action(view.create);
-program.command("delete").description("Delete a View.").action(view.delete);
-
-program.parse(process.argv);
