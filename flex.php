d1611386b0142daf24a13b491feb22001992942f
z-offset <techcyber598@gmail.com>
2020-11-24 11:00:10 -0800

    latest changes

--- flex.php
+++ flex.php
@@ -6,5 +6,5 @@ error_reporting(E_ERROR);
 # Command Line Interface Initializer #
 require_once __DIR__ . '/bin/__Init.php';
 
-# Initialize the Command Line Interface. #
+# Initialize the Command Line Interface.
 Cli::init();
